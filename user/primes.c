96012724c30bfd72dc450ec0f1c86740a9232f16
Evan Wiseman <eewitsevan@gmail.com>
2025-07-13 20:22:40 -0700

    updated primes

--- user/primes.c
+++ user/primes.c
@@ -2,63 +2,49 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-main(int argc, char **argv) {
-  int input[2];
-  pipe(input);
 
-  int pid = fork();
-  if (pid == 0) { // initialize the pipe
-    close(input[0]);
-    for (int i = 2; i <= 35; i++) {
-      write(input[1], &i, sizeof(int));
-    }
-    close(input[1]);
-    exit(0);
-  } else {
-    wait(0);
-    int prime, n;
+void 
+exec_pipe(int fd) {
+  int prime;
+  if(read(fd, &prime, 4) != 4) {
+    close(fd);
+    return;
+  }
+  printf("prime %d\n", prime);
 
-    close(input[1]);
-    if ((n = read(input[0], &prime, sizeof(int))) != sizeof(int)) {
-      close(input[0]);
-      exit(0);
-    }
-
-    printf("prime %d\n", prime);
-
-    int output[2];
-    pipe(output);
-
-    int pid = fork();
-    if(pid == 0) {
-      close(output[0]);
-      
-      int num;
-      while ((n = read(input[0], &num, sizeof(int))) == sizeof(int)) {
-        if (num % prime != 0) {
-          write(output[1], &num, sizeof(int)); 
-        }
-      }
-
-      close(input[0]);
-      close(output[1]);
-      exit(0);
-    } else {
-      wait(0);
-
-      close(input[0]);
-      close(output[1]);
-
-      close(0);
-      dup(output[0]);
-      close(output[0]);
-
-      char *argv[] = {"primes", 0};
-      exec(argv[0], argv);
-
-      fprintf(2, "exec failed\n");
-      exit(1);
+  int p[2];
+  pipe(p);
+  int num;
+  while (read(fd, &num, 4) == 4) {
+    if (num % prime != 0) {
+      write(p[1], &num, 4);
     }
   }
+
+  int pid = fork();
+  if (pid == 0) {
+    close(p[1]);
+    close(fd);
+    exec_pipe(p[0]);
+    close(p[0]);
+  } else {
+    close(p[1]);
+    close(p[0]);
+    close(fd);
+    wait(0);
+  }
+}
+
+int
+main(int *argc, char **argv) {
+  int p[2];
+  pipe(p);
+  for (int i = 2; i < 35; i++) {
+    write(p[1], &i, 4);
+  }
+  close(p[1]);
+  exec_pipe(p[0]);
+  close(p[0]);
+
+  exit(0);
 }
