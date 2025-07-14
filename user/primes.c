f90b916e94481c96cad5493b6a29412d9b3a02cf
Evan Wiseman <eewitsevan@gmail.com>
2025-07-13 20:01:52 -0700

    moved recurrsion to different function for primes

--- user/primes.c
+++ user/primes.c
@@ -2,47 +2,47 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-// Filter logic: reads from input_fd, filters, and spawns next stage
-void
-filter(int input_fd) {
+void filter(int input[]) {
   int prime, n;
 
-  if ((n = read(input_fd, &prime, sizeof(int))) != sizeof(int)) {
-    close(input_fd);
+  close(input[1]);
+  if ((n = read(input[0], &prime, sizeof(int))) != sizeof(int)) {
+    close(input[0]);
     exit(0);
   }
 
   printf("prime %d\n", prime);
 
-  int p[2];
-  pipe(p);
+  int output[2];
+  pipe(output);
 
   int pid = fork();
-  if (pid > 0) {
-    // Parent: filter numbers and write to next pipe
-    close(p[0]);
+  if(pid == 0) {
+    close(output[0]);
+    
     int num;
-    while ((n = read(input_fd, &num, sizeof(int))) == sizeof(int)) {
+    while ((n = read(input[0], &num, sizeof(int))) == sizeof(int)) {
       if (num % prime != 0) {
-        write(p[1], &num, sizeof(int));
+        write(output[1], &num, sizeof(int)); 
       }
     }
-    close(input_fd);
-    close(p[1]);
-    wait(0);
+
+    close(input[0]);
+    close(output[1]);
     exit(0);
   } else {
-    // Child: prepare for exec with next pipe as stdin
-    close(p[1]);
-    close(input_fd);
+    wait(0);
 
-    // Redirect p[0] → fd 0
+    close(input[0]);
+    close(output[1]);
+
     close(0);
-    dup(p[0]);
-    close(p[0]);
+    dup(output[0]);
+    close(output[0]);
 
-    char *args[] = {"sieve", "child", 0};
-    exec(args[0], args);
+    char *argv[] = {"filter", 0};
+    exec(argv[0], argv);
+
     fprintf(2, "exec failed\n");
     exit(1);
   }
@@ -50,28 +50,19 @@ filter(int input_fd) {
 
 int
 main(int argc, char **argv) {
-  if (argc == 1) {
-    // Initial parent: generate numbers 2..35
-    int p[2];
-    pipe(p);
+  int input[2];
+  pipe(input);
 
-    int pid = fork();
-    if (pid > 0) {
-      close(p[0]);
-      for (int i = 2; i <= 35; i++) {
-        write(p[1], &i, sizeof(int));
-      }
-      close(p[1]);
-      wait(0);
-      exit(0);
-    } else {
-      close(p[1]);
-      filter(p[0]);
+  int pid = fork();
+  if (pid == 0) { // initialize the pipe
+    close(input[0]);
+    for (int i = 2; i <= 35; i++) {
+      write(input[1], &i, sizeof(int));
     }
+    close(input[1]);
+    exit(0);
   } else {
-    // Re-exec’d children: read from stdin (which was dup’d from pipe)
-    filter(0);
+    wait(0);
+    filter(input);
   }
-
-  exit(0);
 }
