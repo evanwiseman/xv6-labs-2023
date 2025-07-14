e7f134bd5c8a6fec90788dc86c931c6883d391f2
Evan Wiseman <eewitsevan@gmail.com>
2025-07-13 20:27:17 -0700

    add comments to primes

--- user/primes.c
+++ user/primes.c
@@ -8,12 +8,17 @@ exec_pipe(int fd) {
   int prime;
   if(read(fd, &prime, 4) != 4) {
     close(fd);
-    return;
+    exit(0);
   }
+
+  // output current prime number
   printf("prime %d\n", prime);
 
+  // build right pipe
   int p[2];
   pipe(p);
+
+  // filter prime numbers into right
   int num;
   while (read(fd, &num, 4) == 4) {
     if (num % prime != 0) {
@@ -22,27 +27,37 @@ exec_pipe(int fd) {
   }
 
   int pid = fork();
-  if (pid == 0) {
+  if (pid == 0) { 
+    // close file descriptors
+    // execute on right pipe
     close(p[1]);
     close(fd);
     exec_pipe(p[0]);
     close(p[0]);
   } else {
+    // all work done in parent
+    // wait for children to finish
     close(p[1]);
     close(p[0]);
     close(fd);
     wait(0);
   }
+
+  exit(0);
 }
 
 int
 main(int *argc, char **argv) {
+  // build left pipe
   int p[2];
   pipe(p);
   for (int i = 2; i < 35; i++) {
     write(p[1], &i, 4);
   }
+  
   close(p[1]);
+
+  // find primes
   exec_pipe(p[0]);
   close(p[0]);
 
