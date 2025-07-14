8f2d6d86ce020b554d9d6eee270803f8e9152efa
Evan Wiseman <eewitsevan@gmail.com>
2025-07-13 20:07:56 -0700

    moved function back to main

--- user/primes.c
+++ user/primes.c
@@ -2,52 +2,6 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-void filter(int input[]) {
-  int prime, n;
-
-  close(input[1]);
-  if ((n = read(input[0], &prime, sizeof(int))) != sizeof(int)) {
-    close(input[0]);
-    exit(0);
-  }
-
-  printf("prime %d\n", prime);
-
-  int output[2];
-  pipe(output);
-
-  int pid = fork();
-  if(pid == 0) {
-    close(output[0]);
-    
-    int num;
-    while ((n = read(input[0], &num, sizeof(int))) == sizeof(int)) {
-      if (num % prime != 0) {
-        write(output[1], &num, sizeof(int)); 
-      }
-    }
-
-    close(input[0]);
-    close(output[1]);
-    exit(0);
-  } else {
-    wait(0);
-
-    close(input[0]);
-    close(output[1]);
-
-    close(0);
-    dup(output[0]);
-    close(output[0]);
-
-    char *argv[] = {"primes", 0};
-    exec(argv[0], argv);
-
-    fprintf(2, "exec failed\n");
-    exit(1);
-  }
-}
-
 int
 main(int argc, char **argv) {
   int input[2];
@@ -63,6 +17,48 @@ main(int argc, char **argv) {
     exit(0);
   } else {
     wait(0);
-    filter(input);
+    int prime, n;
+
+    close(input[1]);
+    if ((n = read(input[0], &prime, sizeof(int))) != sizeof(int)) {
+      close(input[0]);
+      exit(0);
+    }
+
+    printf("prime %d\n", prime);
+
+    int output[2];
+    pipe(output);
+
+    int pid = fork();
+    if(pid == 0) {
+      close(output[0]);
+      
+      int num;
+      while ((n = read(input[0], &num, sizeof(int))) == sizeof(int)) {
+        if (num % prime != 0) {
+          write(output[1], &num, sizeof(int)); 
+        }
+      }
+
+      close(input[0]);
+      close(output[1]);
+      exit(0);
+    } else {
+      wait(0);
+
+      close(input[0]);
+      close(output[1]);
+
+      close(0);
+      dup(output[0]);
+      close(output[0]);
+
+      char *argv[] = {"primes", 0};
+      exec(argv[0], argv);
+
+      fprintf(2, "exec failed\n");
+      exit(1);
+    }
   }
 }
